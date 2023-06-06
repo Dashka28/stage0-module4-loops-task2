f5e3474ac51da300427384c45631c3b5220e436f
Dashka28 <131460708+Dashka28@users.noreply.github.com>
2023-06-06 16:36:56 +0600

    Update WordsBuilder.java

--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -2,6 +2,9 @@ package school.mjc.stage0.loops.task2;
 
 public class WordsBuilder {
     public void buildPhrase(char... chars) {
+        char[] charsArr = chars;
+        String str = new String(charsArr);
+        System.out.println(str);
 
     }
 }
